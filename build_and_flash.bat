81fc8fb023d711b399d1cfde266022e50647453f
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-06 14:23:56 +0700

    Update build_and_flash.bat

--- build_and_flash.bat
+++ build_and_flash.bat
@@ -1,1 +1,12 @@
-
+cd hello_world
+rmdir /s /q build
+mkdir build
+cd build
+cmake -G Ninja ..
+ninja
+ninja flash
+set ESPPORT=COM9
+set ESPBAUD=460800
+ninja flash
+set ESPBAUD=115200
+ninja monitor
