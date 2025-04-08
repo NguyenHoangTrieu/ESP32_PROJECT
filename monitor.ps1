b19892a562344cf7e76061df74a90d4a08432c6c
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-08 18:55:42 +0700

    Update monitor.ps1

--- monitor.ps1
+++ monitor.ps1
@@ -1,3 +1,10 @@
-$proc = Start-Process "idf.py" -ArgumentList "monitor -p COM9" -NoNewWindow -PassThru
-Start-Sleep -Seconds 30
-Stop-Process -Id $proc.Id
+$proc = Start-Process "idf.py" -ArgumentList "monitor -p COM9" -PassThru
+$timeout = 30
+$pid = $proc.Id
+
+try {
+    Wait-Process -Id $pid -Timeout $timeout
+} catch {
+    # Nếu quá timeout mà vẫn chạy, thì dừng lại
+    Stop-Process -Id $pid -Force
+}
