d5048798decf6d6f3298249837300b5ffaba740f
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-08 18:40:06 +0700

    Update

--- build_and_flash.bat
+++ build_and_flash.bat
@@ -1,3 +1,4 @@
+call C:\Users\MSI-VN\esp\esp-idf\export.bat
 cd hello_world
 rmdir /s /q build
 mkdir build
@@ -9,3 +10,4 @@ set ESPPORT=COM9
 set ESPBAUD=460800
 ninja flash
 set ESPBAUD=115200
+powershell -Command "$proc = Start-Process 'idf.py' -ArgumentList '-p COM9 monitor' -NoNewWindow -PassThru; Start-Sleep -Seconds 30; Stop-Process -Id $proc.Id"
