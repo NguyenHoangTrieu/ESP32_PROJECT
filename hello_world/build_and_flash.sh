e075ca1823d55b4b260f86675c97d9574e2e2f30
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-23 17:06:20 +0700

    Update build_and_flash.sh

--- hello_world/build_and_flash.sh
+++ hello_world/build_and_flash.sh
@@ -1,4 +1,10 @@
-source "$HOME/esp/esp-idf/export.sh"
+source ~/.profile
+source "$EXPORT_ENV"
+# Check if the environment variable is set
+if [ -z "$EXPORT_ENV" ]; then
+    echo "Error: EXPORT_ENV is not set."
+    exit 1
+fi
 rm -rf build
 mkdir build
 cd build
