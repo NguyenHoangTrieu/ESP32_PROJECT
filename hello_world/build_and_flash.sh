70fd2412789478052e4124ecc681d3f1618887ac
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-23 18:01:42 +0700

    Update build_and_flash.sh

--- hello_world/build_and_flash.sh
+++ hello_world/build_and_flash.sh
@@ -3,6 +3,11 @@ sudo chmod o+rx /home
 sudo chmod o+rx /home/npvinh
 sudo chmod o+rx /home/npvinh/esp
 sudo chmod o+rx /home/npvinh/esp/esp-idf
+if [ ! -d "$HOME/.espressif/python_env" ]; then
+    echo ">>> Installing ESP-IDF Python environment..."
+    cd $HOME/esp/esp-idf
+    ./install.sh
+fi
 source /home/npvinh/esp/esp-idf/export.sh
 rm -rf build
 mkdir build
