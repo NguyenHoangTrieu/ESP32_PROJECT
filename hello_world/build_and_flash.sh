eb9d9bc712b8500ea5224409f8a5325433911317
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-23 18:08:06 +0700

    Update build_and_flash.sh

--- hello_world/build_and_flash.sh
+++ hello_world/build_and_flash.sh
@@ -4,6 +4,7 @@ sudo chmod o+rx /home/npvinh
 sudo chmod o+rx /home/npvinh/esp
 sudo chmod o+rx /home/npvinh/esp/esp-idf
 IDF_PATH="/home/npvinh/esp/esp-idf"
+export IDF_PATH="$IDF_PATH"
 source /home/npvinh/esp/esp-idf/export.sh
 rm -rf build
 mkdir build
