bdb168091e90ea695e828e7b2bbe780bcde86197
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-23 17:50:56 +0700

    Update build_and_flash.sh

--- hello_world/build_and_flash.sh
+++ hello_world/build_and_flash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-chmod +r /home/npvinh/esp/esp-idf/export.sh
-source /home/npvinh/esp/esp-idf/export.sh
+sudo chmod +x /home/npvinh/esp/esp-idf/export.sh
+sudo source /home/npvinh/esp/esp-idf/export.sh
 rm -rf build
 mkdir build
 cd build
