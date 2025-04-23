14c83039e6dc3faea79d6a05e8397a62b38c8eb5
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-23 17:33:13 +0700

    Update build_and_flash.sh

--- hello_world/build_and_flash.sh
+++ hello_world/build_and_flash.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-chmod +x /home/npvinh/esp/esp-idf/export.sh
+sudo chmod +x /home/npvinh/esp/esp-idf/export.sh
 export EXPORT_ENV="/home/npvinh/esp/esp-idf/export.sh"
-source "$EXPORT_ENV"
-if [ -f "$EXPORT_ENV" ]; then
-    source "$EXPORT_ENV"
-else
-    echo "ESP-IDF export script not found at $EXPORT_ENV"
-    exit 1
-fi
+sudo /bin/bash /home/npvinh/esp/esp-idf/export.sh
 rm -rf build
 mkdir build
 cd build
